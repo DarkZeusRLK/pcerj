07a0b795a0f5b8d4c39615c05825ff96ae926430
unknown <fernandesjoaopedro622@gmail.com>
2025-01-08 18:13:13 -0300

    att

--- index.php
+++ index.php
@@ -15,10 +15,9 @@
 
 <body>
 
-  <!-- Barra de navegação lateral -->
   <div class="sidebar">
     <div class="logo-container">
-      <a href="index.php"><img src="Imagens/image (1).png" alt="Logo Principal" class="logo-principal"> <!-- Logo Principal --></a>
+      <a href="index.php"><img src="Imagens/image (1).png" alt="Logo Principal" class="logo-principal"></a>
     </div>
     <h2>Polícia Civil</h2>
     <ul>
@@ -26,29 +25,36 @@
         <a href="#home">Home</a>
       </li>
       <li>
-        <a href="contagem_gem.php">GEM</a>
-        <img src="Imagens/GEM_IMPACT_2.png" alt="Logo 2" class="logo-pequena"> <!-- Logo Pequena -->
+        <a href="https://calculadora-penal.revoadarj.com.br">Calculadora Penal</a>
       </li>
       <li>
-        <a href="contagem_saer.php">SAER</a>
-        <img src="Imagens/saerpcrj-removebg-preview.png" alt="Logo 3" class="logo-pequena"> <!-- Logo Pequena -->
+        <a href="imortais.html">Imortais da Academia de Letras</a>
       </li>
       <li>
         <a href="#contact">CORE</a>
-        <img src="Imagens/Core_logo.png" alt="Logo 4" class="logo-pequena"> <!-- Logo Pequena -->
+        <img src="Imagens/Core_logo.png" alt="Logo 4" class="logo-pequena">
       </li>
+      <li>
+        <a href="cgpc.php">CGPC</a>
+        <img src="Imagens/Logo_CGPC" alt="Logo 3" class="logo-pequena">
+      </li>
+      <li>
+        <a href="contagem_saer.php">SAER</a>
+        <img src="Imagens/saerpcrj-removebg-preview.png" alt="Logo 3" class="logo-pequena">
+      </li>
+      <li>
+        <a href="contagem_gem.php">GEM</a>
+        <img src="Imagens/GEM_IMPACT_2.png" alt="Logo 2" class="logo-pequena">
+        < </li>
+
     </ul>
   </div>
 
-  <!-- Conteúdo principal -->
+
   <div class="main-content">
-    <!-- Galeria de imagens no topo -->
-    <!-- Mensagem dinâmica acima da galeria -->
     <div class="dynamic-message">
       <h2><span id="typed-message"></span></h2>
     </div>
-    <!-- Galeria de imagens no topo -->
-    <!-- Galeria de imagens no topo -->
     <div class="image-gallery">
       <div class="gallery-container">
         <div class="gallery-item" style="background-image: url('Imagens/Formação1.png');"></div>
@@ -70,12 +76,12 @@
       const totalItems = galleryItems.length;
 
       function scrollGallery(direction) {
-        const itemWidth = galleryItems[0].clientWidth; // Largura do item
+        const itemWidth = galleryItems[0].clientWidth;
         currentIndex += direction;
 
-        // Verificar limites
+
         if (currentIndex < 0) {
-          currentIndex = totalItems - 4; // Ajustar para girar em 4 imagens
+          currentIndex = totalItems - 4;
         } else if (currentIndex > totalItems - 4) {
           currentIndex = 0;
         }
@@ -83,63 +89,125 @@
         galleryContainer.style.transform = `translateX(${-currentIndex * itemWidth}px)`;
       }
 
-      // Troca automática das imagens
+
       function autoScrollGallery() {
-        scrollGallery(1); // Avançar uma imagem
+        scrollGallery(1);
       }
 
-      let autoScroll = setInterval(autoScrollGallery, 4000); // Troca a cada 4 segundos
+      let autoScroll = setInterval(autoScrollGallery, 4000);
 
-      // Parar a troca automática ao passar o mouse
+
       galleryContainer.addEventListener('mouseover', () => clearInterval(autoScroll));
 
-      // Retomar a troca automática ao remover o mouse
+
       galleryContainer.addEventListener('mouseout', () => autoScroll = setInterval(autoScrollGallery, 4000));
 
     </script>
 
     <section id="home" class="welcome">
-      <h1>Bem-vindo ao Sistema de Contagem de Patrulha</h1>
-      <p>Este sistema foi criado com intuito de facilitar e minimizar os esforços ao fazer os relatórios de patrulhas semanais dos grupamentos e guarnições.</p>
+      <h1>Bem-vindo ao Memorial da Polícia Civil do Estado do Rio de Janeiro.</h1>
+      <p>Este sistema foi criado com intuito de eternizar e lembrar de todos os políciais que fizeram desta polícia ser o que ela é hoje, seu trabalho jamais será esquecido e está cravado como lembrança em nossas memórias e corações.
+      </p>
     </section>
+    <section id="delegados" class="delegates-section">
+      <h2>Gestão Echo 1</h2>
+      <div class="delegate-container">
 
-    <!-- Container principal de cards com fundo estilizado -->
-    <!-- Container principal de cards com fundo estilizado -->
-    <div class="cards-wrapper">
-      <div class="card-container">
-        <div class="card">
-          <div class="card-background" style="background-image: url('Imagens/GEM_IMPACT_2.png');"></div>
-          <div class="card-content">
-            <h3>G.E.M</h3>
-            <p>Fazer relatório de Contagem de Patrulha Semanal.</p>
-            <a href="contagem_gem.php" class="btn">Fazer Contagem</a>
-          </div>
+        <div class="delegate-card general">
+          <img src="Imagens/delegado_geral1.jpg" alt="Foto do Delegado Geral" class="delegate-photo">
+          <h3>Delegado Geral: Decantas Tenebras (Dede)</h3>
+          <p>Breve descrição do Delegado Geral e suas contribuições.</p>
         </div>
 
-        <div class="card">
-          <div class="card-background" style="background-image: url('Imagens/saerpcrj-removebg-preview.png');"></div>
-          <div class="card-content">
-            <h3>S.A.E.R</h3>
-            <p>Fazer relatório de Contagem de Patrulha Semanal.</p>
-            <a href="#" class="btn">Fazer Contagem</a>
-          </div>
+        <div class="delegate-card adjunto">
+          <img src="Imagens/delegado_adjunto1.jpg" alt="Foto do Delegado Adjunto" class="delegate-photo">
+          <h3>Delegado Adjunto: Henry Romanov Tenebras (Karin)</h3>
+          <p>Descrição sobre o Delegado Adjunto e sua atuação.</p>
         </div>
-
-        <div class="card">
-          <div class="card-background" style="background-image: url('Imagens/Core_logo.png');"></div>
-          <div class="card-content">
-            <h3>C.O.R.E</h3>
-            <p>Fazer relatório de Contagem de Metas Semanais.</p>
-            <a href="#" class="btn">Fazer Contagem</a>
-          </div>
+        <div class="delegate-card adjunto">
+          <img src="Imagens/delegado_adjunto2.jpg" alt="Foto do Delegado Adjunto" class="delegate-photo">
+          <h3>Delegado Adjunto: Samara Tenebras (Sammy)</h3>
+          <p>Descrição sobre o Delegado Adjunto e sua atuação.</p>
         </div>
       </div>
-    </div>
+
+
+      <h2>Gestão Echo 2</h2>
+      <div class="delegate-container">
+        <div class="delegate-card general">
+          <img src="Imagens/delegado_geral2.jpg" alt="Foto do Delegado Geral" class="delegate-photo">
+          <h3>Delegado Geral: Decantas Tenebras (Dede)</h3>
+          <p>Breve descrição do Delegado Geral e suas contribuições.</p>
+        </div>
+        <div class="delegate-card adjunto">
+          <img src="Imagens/delegado_adjunto3.jpg" alt="Foto do Delegado Adjunto" class="delegate-photo">
+          <h3>Delegado Adjunto: Henry Romanov Tenebras (Karin)</h3>
+          <p>Descrição sobre o Delegado Adjunto e sua atuação.</p>
+        </div>
+        <div class="delegate-card adjunto">
+          <img src="Imagens/delegado_adjunto4.jpg" alt="Foto do Delegado Adjunto" class="delegate-photo">
+          <h3>Delegado Adjunto: John Romanov Tenebras (Dark)</h3>
+          <p>Descrição sobre o Delegado Adjunto e sua atuação.</p>
+        </div>
+      </div>
+
+
+      <h2>Gestão Echo 3</h2>
+      <div class="delegate-container">
+        <div class="delegate-card general">
+          <img src="Imagens/delegado_geral3.jpg" alt="Foto do Delegado Geral" class="delegate-photo">
+          <h3>Delegado Geral: Decantas Tenebras (Dede)</h3>
+          <p>Breve descrição do Delegado Geral e suas contribuições.</p>
+        </div>
+        <div class="delegate-card adjunto">
+          <img src="Imagens/delegado_adjunto5.jpg" alt="Foto do Delegado Adjunto" class="delegate-photo">
+          <h3>Delegado Adjunto: John Romanov Tenebras (Dark)</h3>
+          <p>Descrição sobre o Delegado Adjunto e sua atuação.</p>
+        </div>
+        <div class="delegate-card adjunto">
+          <img src="Imagens/delegado_adjunto6.jpg" alt="Foto do Delegado Adjunto" class="delegate-photo">
+          <h3>Delegado Adjunto: Marcelo Tenebras (Marcelin)</h3>
+          <p>Descrição sobre o Delegado Adjunto e sua atuação.</p>
+        </div>
+      </div>
+    </section>
+
+    <section class="grupamentos">
+      <h2 class="text-grup">Memorial dos Grupamentos</h2>
+      <div class="grupamento">
+        <img src="Imagens/Core_logo.png" alt="CORE">
+        <div class="descricao">
+          <h3>CORE</h3>
+          <p>A Coordenadoria de Recursos Especiais é a unidade de elite da Polícia Civil, especializada em ações táticas de alto risco. Suas missões incluem intervenções em situações críticas, como combate ao terrorismo, resgate de reféns e operações contra organizações criminosas altamente armadas. O CORE é formado por profissionais rigorosamente treinados em diversas áreas, como negociação, explosivos e uso de armamento pesado, garantindo a segurança da população em cenários extremos.</p>
+        </div>
+      </div>
+      <div class="grupamento">
+        <img src="Imagens/Logo_CGPC.png" alt="CGPC">
+        <div class="descricao">
+          <h3>CGPC</h3>
+          <p>A CGPC está diretamente vinculada a inquéritos, mandados, investigações e procedimentos internos da PCERJ, também sendo responsável pela padronização e fiscalização dos registros prisionais e periciais, bem como sendo o setor competente para identificar/corrigir falhas nos procedimentos policiais e esquemas de corrupção de todas as matrizes.</p>
+        </div>
+      </div>
+      <div class="grupamento">
+        <img src="Imagens/saerpcrj-removebg-preview.png" alt="SAER">
+        <div class="descricao">
+          <h3>SAER</h3>
+          <p>O Serviço Aeropolicial oferece suporte aéreo essencial para as operações da Polícia Civil. Com aeronaves modernas e uma equipe altamente capacitada, o SAER é responsável por ações como patrulhamento aéreo, transporte de equipes táticas, evacuação médica e resgate em áreas de difícil acesso. A mobilidade e a visão estratégica proporcionadas pelas operações aéreas tornam o SAER um componente indispensável em missões de segurança pública.</p>
+        </div>
+      </div>
+      <div class="grupamento">
+        <img src="Imagens/GEM_IMPACT_2.png" alt="GEM">
+        <div class="descricao">
+          <h3>GEM</h3>
+          <p>O Grupamento Especial de Motocicletas é especializado em patrulhamento e operações rápidas em áreas urbanas e de difícil acesso. Com motocicletas ágeis e pilotos experientes, o GEM atua em ações de combate ao crime, escoltas e intervenções táticas. Sua capacidade de se deslocar rapidamente em cenários de tráfego intenso e terrenos desafiadores faz do GEM uma força essencial para garantir a segurança e a ordem pública em situações dinâmicas.</p>
+        </div>
+      </div>
+    </section>
 
   </div>
 
 
-  <!-- Rodapé -->
+
   <footer class="footer">
     <div class="footer-content">
       <div class="footer-logo">
@@ -166,18 +234,27 @@
     document.addEventListener("DOMContentLoaded", function () {
       var typed = new Typed("#typed-message", {
         strings: ["Bem-vindo à Polícia Civil de Revoada ", "Protegendo a cidade com honra", "A justiça é nossa missão", "Explore nossos serviços!"],
-        typeSpeed: 50, // Velocidade de digitação
-        backSpeed: 25, // Velocidade ao apagar
-        backDelay: 2000, // Atraso antes de começar a apagar
-        loop: true, // Repetir o ciclo de mensagens
-        showCursor: true, // Mostrar o cursor piscando
-        cursorChar: "|", // Estilo do cursor
+        typeSpeed: 50,
+        backSpeed: 25,
+        backDelay: 2000,
+        loop: true,
+        showCursor: true,
+        cursorChar: "|",
         autoInsertCss: true
       });
     });
   </script>
 
 </body>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+<script>
+  AOS.init({
+    duration: 1200,
+    once: true
+  });
+</script>
+
 <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
 
 
